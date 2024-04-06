d47472dce7a24194d9cf0da6a859de775b97b0b1
Cody <codylau6233@hotmail.com>
2024-04-05 18:53:00 -0700

    feat: Admin can create new category.

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resource :categories, only: [:show, :new]
+    resource :categories, only: [:show, :new, :create]
   end
 
   get 'about', to: 'about#index'
